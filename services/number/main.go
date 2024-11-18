3aab74971a4a4effa080f6334a8d824b77291465
Leon Steinhäuser <lsteinha@redhat.com>
2024-11-18 09:23:27 +0100

    chore: add debug logs

--- services/number/main.go
+++ services/number/main.go
@@ -5,13 +5,29 @@ import (
 	"log/slog"
 	"math/rand/v2"
 	"net/http"
+	"os"
 )
+
+func init() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
+	slog.SetDefault(logger)
+}
 
 func main() {
 	// add routes
-	http.HandleFunc("GET /number", getNumber)
+	http.HandleFunc("GET /number", logMiddleware(getNumber))
 	slog.Info("starting server on port 8081")
 	http.ListenAndServe(":8081", nil)
+}
+
+func logMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.DebugContext(r.Context(), "incoming request", "method", r.Method, "url", r.URL.Path, "remote", r.RemoteAddr)
+		next(w, r)
+	}
 }
 
 func getNumber(w http.ResponseWriter, r *http.Request) {
--- services/view/main.go
+++ services/view/main.go
@@ -12,9 +12,17 @@ var (
 	envNumberServiceURL = os.Getenv("NUMBER_SERVICE_URL")
 )
 
+func init() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
+	slog.SetDefault(logger)
+}
+
 func main() {
 	// add routes
-	http.HandleFunc("GET /", getMain)
+	http.HandleFunc("GET /", logMiddleware(getMain))
 	slog.Info("starting server on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
@@ -37,9 +45,17 @@ var (
 	websiteTemplate = template.Must(template.New("website").Parse(websiteHTML))
 )
 
+func logMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.DebugContext(r.Context(), "incoming request", "method", r.Method, "url", r.URL.Path, "remote", r.RemoteAddr)
+		next(w, r)
+	}
+}
+
 func getMain(w http.ResponseWriter, r *http.Request) {
 	rsp, err := http.Get(envNumberServiceURL + "/number")
 	if err != nil || rsp.StatusCode != http.StatusOK {
+		slog.DebugContext(r.Context(), "http upstream error: unable to get number from number service", "number_service_url", envNumberServiceURL+"/number", "error", err)
 		http.Error(w, "http upstream error: could not get number from number service", http.StatusInternalServerError)
 		return
 	}
@@ -48,17 +64,20 @@ func getMain(w http.ResponseWriter, r *http.Request) {
 	payload := map[string]uint64{}
 	err = json.NewDecoder(rsp.Body).Decode(&payload)
 	if err != nil {
+		slog.DebugContext(r.Context(), "http upstream error: could not decode number from number service", "error", err)
 		http.Error(w, "http upstream error: could not decode number from number service", http.StatusInternalServerError)
 		return
 	}
 
 	if _, ok := payload["number"]; !ok {
+		slog.DebugContext(r.Context(), "http upstream error: number key not found in response from number service")
 		http.Error(w, "http upstream error: number key not found in response from number service", http.StatusInternalServerError)
 		return
 	}
 
 	err = websiteTemplate.Execute(w, payload)
 	if err != nil {
+		slog.DebugContext(r.Context(), "could not render website", "error", err)
 		http.Error(w, "could not render website", http.StatusInternalServerError)
 		return
 	}
